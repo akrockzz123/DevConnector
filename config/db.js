97a2e0d47a150c90a7abe5f6bc0a768e8138dcd4
akrockzz123 <aniketkumar1601@gmail.com>
2021-08-18 09:19:27 +0530

    connected to mongodb

---
+++ config/db.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const config = require('config')
+
+const db = config.get('mongoURI')
+
+console.log(db)
+
+
+const connectDB = async () => {
+    try {
+        console.log('connecting....')
+        await mongoose.connect(db)
+        
+        console.log("MongoDB connected .....")
+    } catch(err) {
+        console.log(err.message)
+
+        // exit the process
+
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const { response } = require('express')
 const express = require('express')
 
+const connectDB = require('./config/db')
+
 const app = express()
+
+// connect to the data base
+
+connectDB()
 
 app.get('/', (req,res) => res.send('API is running on port 6000'))
 
