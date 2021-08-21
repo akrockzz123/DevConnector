a8dd846b3a30bb0f3e8cc8ad1844f41f65b8ae92
akrockzz123 <aniketkumar1601@gmail.com>
2021-08-21 16:27:40 +0530

    added delete route and update route for experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -194,7 +194,7 @@ router.delete('/', auth,async (req,res) => {
 })
 
 
-// @route PUT api/profile/user/:user_id
+// @route PUT api/profile/experience
 // @desc add profile experience
 // @access Private
 
@@ -256,6 +256,36 @@ router.put('/experience',[auth,[
         }
         console.log(err.message)
         res.status(500).send('Server error')
+    
+})
+
+
+// @route DELETE api/profile/experience/:exp_id
+// @desc DELETE EXPERIENCE FROM PROFILE
+// @access Private
+
+router.delete('/experience/:exp_id', auth,async (req,res) => {
+
+    try {
+        const profile = await Profile.findOne({user: req.user.id})
+
+        //get remove index
+
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id)
+
+        profile.experience.splice(removeIndex,1)
+
+        await profile.save()
+
+        res.json({profile})
+
+    } catch(err) {
+
+        console.log(err.message)
+        res.status(500).send('Server Error')
+    }
+
+        
     
 })
 
